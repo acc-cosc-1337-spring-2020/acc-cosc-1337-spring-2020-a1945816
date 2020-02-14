a01cc2698d4a96cc347c5de3b2c7b177ca400132
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-02-13 21:51:14 -0600

    remove

--- out/build/x64-Debug/test/examples_test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/zoobu/source/repos/acc-cosc-1337-spring-2020-a1945816/test/examples_test
-# Build directory: C:/Users/zoobu/source/repos/acc-cosc-1337-spring-2020-a1945816/out/build/x64-Debug/test/examples_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("01_module_test")
-subdirs("02_module_test")
