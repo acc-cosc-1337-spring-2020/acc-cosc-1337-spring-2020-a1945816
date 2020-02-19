00d0a094e514c1068c3bb80916b2cac7d8794ad2
a1945816 <andrew.thompson2@g.austincc.edu>
2020-02-18 19:04:50 -0600

    made a loop vector functions that show reference

--- src/examples/03_module/06_vectors/vec.cpp
+++ src/examples/03_module/06_vectors/vec.cpp
@@ -18,6 +18,13 @@ Write code for function loop_vector_w_index with a vector of int pass by value p
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows: 9 10 99 5  67
 */
+void loop_vector_w_index(std::vector<int> nums)
+{
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		std::cout << nums[i] << "\n";
+	}
+}
 
 
 
@@ -26,7 +33,13 @@ Write code for function loop_vector_w_index with a vector of int pass by referen
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows : 9 10 99 5  67
 */
-
+void loop_vector_w_index_ref(std::vector<int> &nums)
+{
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		std::cout << nums[i] << "\n";
+	}
+}
 
 /*
 Write code for function loop_vector_w_index with a vector of int const pass by reference parameter.
@@ -34,3 +47,35 @@ The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows : 9 10 99 5  67
 */
 
+void loop_vector_w_index_ref_const(const std::vector<int> &nums)
+{
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		std::cout << nums[i] << "\n";
+	}
+
+}
+
+void loop_vector_w_auto(std::vector<int>& nums)
+{
+	for (auto i : nums)
+	{
+		i = 0;
+	}
+}
+
+void loop_vector_w_auto_const(const std::vector<int>& nums)
+{
+	for (auto i : nums)
+	{
+		i = 0;
+	}
+}
+
+void loop_vector_w_auto_ref(std::vector<int>& nums)
+{
+	for (auto &i : nums)
+	{
+		i = 0;
+	}
+}
