1bdf3c1ed466b51caf9f9b9737097dad2f93377e
a1945816 <andrew.thompson2@g.austincc.edu>
2020-02-27 19:26:27 -0600

    Made a class that finds the area of a rectangle then displays using a vector to store values

--- src/classwork/05_assign/main.cpp
+++ src/classwork/05_assign/main.cpp
@@ -1,5 +1,8 @@
 //write include statemetns
-
+#include "rectangle.h"
+#include <vector>
+#include<iostream>
+using std::vector;	using std::cout;
 /*
 Create a vector of rectangles
 Add 3 Rectangle classes to the vector:
@@ -12,6 +15,18 @@ Iterate the vector and display the Area for each Rectangle on one line and the t
 */
 int main()
 {
-	
+	vector<Rect> rectangle;
+	Rect area(4, 5);
+	Rect area1(10, 10);
+	Rect area2(100, 10);
+
+	rectangle.push_back(area);
+	rectangle.push_back(area1);
+	rectangle.push_back(area2);
+
+	for (int i = 0; i < rectangle.size(); ++i)
+	{
+		cout << rectangle[i].get_area() << "\n";
+	}
 	return 0;
 }
