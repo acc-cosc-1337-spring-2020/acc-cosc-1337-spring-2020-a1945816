c110eefd9c73cebb8efec9f0e7631f9d7e57947f
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-03-10 16:02:05 -0500

    hw6 grading

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -3,12 +3,10 @@
 
 void TicTacToe::start_game(string first_player)
 {
-	player = first_player;
-	int position = 0;
-	if (player == "X" || player == "O")
+	if (first_player == "X" || first_player == "O")
 	{
 		
-		mark_board(position);
+		player = first_player;
 	}
 	
 	else
@@ -19,67 +17,16 @@ void TicTacToe::start_game(string first_player)
 
 void TicTacToe::mark_board(int position)
 {
-	if (player == "O" || player == "X")
+	if (position < 1 || position > 9)
 	{
-		string choice = "no";
-		do
-		{
-			cout << "What position would player "<< player << " like to mark?: ";
-			cin >> position;
-			
-			if (position >= 1 && position <= 9)
-			{
-				string pos1;
-				string pos2;
-				string pos3;
-				string pos4;
-				string pos5;
-				string pos6;
-				string pos7;
-				string pos8;
-				string pos9;
-
-				switch (position)
-				{
-				case 1:
-					pos1 = player;
-				case 2:
-					pos2 = player;
-				case 3:
-					pos3 = player;
-				case 4:
-					pos4 = player;
-				case 5:
-					pos5 = player;
-				case 6:
-					pos6 = player;
-				case 7:
-					pos7 = player;
-				case 8:
-					pos8 = player;
-				case 9:
-					pos9 = player;
-				default:
-					break;
-				}
-
-			}
-
-			else
-			{
-				throw Error("Position must be 1 to 9");
-			}
-			set_next_player();
-			cout << "Would player " << player << " like to concede? (yes or no): ";
-			cin >> choice;
-		} while (choice == "no");
+		throw Error("Position must be between 1 and 9.");
 	}
 
-	else 
+	if (player == "")
 	{
-		throw Error("Must start game first.");
+		throw Error(" Must start game first.");
 	}
-
+	set_next_player();
 }
 
 
