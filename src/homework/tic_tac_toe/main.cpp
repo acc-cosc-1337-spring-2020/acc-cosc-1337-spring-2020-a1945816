a613e756d802bbd5ff19e0c7059ab566a0a48833
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-04-05 21:58:43 -0500

    hw7 grading-main incorrect

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -2,28 +2,46 @@
 int main() 
 {
 	TicTacToe game;
-	int position;
+	int position, cont;
 	string first_player{""};
-	cout << "What symbol would you like to start as Enter a Capital X or O: ";
-	cin >> first_player;
-	try
-	{
-		game.start_game(first_player);
-		while(game.game_over() == false)
-		{
-			cout << "choose a position between 1 and 9: " << "\n";
-			cin >> position;
 
-			game.mark_board(position);
-			game.display_board();
-		}
-	}
-	catch (Error e)
+	do
 	{
-		cout << e.get_message() << "\n";
-	}
-	
-	
+		while (!(first_player == "X" || first_player == "O"))
+		{
+			try
+			{
+				cout << "What symbol would you like to start as Enter a Capital X or O: ";
+				cin >> first_player;
+			}
+			catch (Error e)
+			{
+				cout << e.get_message();
+			}
+		}
+
+		game.start_game(first_player);
+
+		try
+		{
+			while (game.game_over() == false)
+			{
+				cout << "choose a position between 1 and 9: " << "\n";
+				cin >> position;
+
+				game.mark_board(position);
+				game.display_board();
+			}
+		}
+		catch (Error e)
+		{
+			cout << e.get_message() << "\n";
+		}
+
+		cout << "Enter 1 to play again";
+		cin >> cont;
+
+	} while (cont == 1);
 
 
 
