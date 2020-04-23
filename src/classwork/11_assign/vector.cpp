b395723daf6d261366bc3b0aea5ada20ec74c0cf
andrew thompson <andrew.thompson2@g.austincc.edu>
2020-04-23 18:26:43 -0500

    vector constructors

--- src/classwork/11_assign/vector.cpp
+++ src/classwork/11_assign/vector.cpp
@@ -1,1 +1,49 @@
 #include "vector.h"
+#include <iostream>
+/*
+Initialize nums to size array.
+Initialize each array element to 0.
+*/
+Vector::Vector(size_t sz)
+	: size{ sz }, nums{new int [sz]}
+{
+	for (size_t i = 0; i < sz; ++i)
+	{
+		nums[i] = 0;
+	}
+}
+/*
+Copy v.size to new class
+create new dynamic memory array.
+initialize array elements to the v.nums array values
+*/
+Vector::Vector(const Vector & v)
+	: size{ v.size }, nums{new int[v.size]}
+{
+	for (size_t i = 0; i < size; ++i)
+	{
+		nums[i] = v[i];
+	}
+}
+
+
+/*
+Release dynamic memory
+Deallocate memory
+*/
+Vector::~Vector()
+{
+	std::cout << "\nrelease memory \n";
+	delete[] nums;
+}
+
+
+//==============================================
+//free function
+void use_vector()
+{
+	Vector* v1 = new Vector(3);
+	delete v1;
+	v1 = nullptr;
+
+}
