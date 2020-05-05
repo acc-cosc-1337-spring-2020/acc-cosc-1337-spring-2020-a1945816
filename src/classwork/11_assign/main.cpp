d2582b3996a086febb211b60fba88940a207288a
andrew thompson <andrew.thompson2@g.austincc.edu>
2020-05-05 18:50:59 -0500

    My vector turned into a template and updated test cases

--- src/classwork/11_assign/main.cpp
+++ src/classwork/11_assign/main.cpp
@@ -1,11 +1,16 @@
 #include "vector.h"
 #include<iostream>
-
+#include <vector>
 int main()
 {
+	
+	
+	Vector<int> v1(3);
 
-	Vector v1(3);
-	v1 = get_vector();
+	for (size_t i = 0; i < v1.Size; ++i)
+	{
+		std::cout << v1[i] << "\n";
+	}
 
 	return 0;
 }
